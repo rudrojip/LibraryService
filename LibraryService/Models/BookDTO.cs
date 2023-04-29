24bcd1597fb8d00de3565db34db90e4073f610fc
rpraneeth <rpraneeth@mirabeltechnologies.com>
2023-04-29 13:18:55 +0530

    Add project files.

---
+++ LibraryService/Models/BookDTO.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace LibraryService.Models
+{
+    public class BookDTO
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("author")]
+        public string? Author { get; set; }
+        [BsonElement("country")]
+        public string? Country { get; set; }
+        [BsonElement("imageLink")]
+        public string? ImageLink { get; set; }
+        [BsonElement("language")]
+        public string? Language { get; set; }
+        [BsonElement("link")]
+        public string? Link { get; set; }
+        [BsonElement("pages")]
+        public int Pages { get; set; }
+        [BsonElement("title")]
+        public string? Title { get; set; }
+        [BsonElement("year")]
+        public int Year { get; set; }
+        [BsonElement("stockLeft")]
+        public int StockLeft { get; set; }
+        [BsonElement("price")]
+        public int Price { get; set; }
+
+    }
+}
---
+++ LibraryService/Services/BookService.cs
@@ -0,0 +1,43 @@
+﻿using LibraryService.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace LibraryService.Services
+{
+
+
+    public class BooksService
+    {
+        private readonly IMongoCollection<BookDTO> _booksCollection;
+
+        public BooksService(
+            IOptions<LibraryDatabaseSettings> LibraryDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                LibraryDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                LibraryDatabaseSettings.Value.DatabaseName);
+
+            _booksCollection = mongoDatabase.GetCollection<BookDTO>(
+                LibraryDatabaseSettings.Value.BooksCollectionName);
+        }
+
+        public async Task<List<BookDTO>> GetAsync() =>
+            await _booksCollection.Find(_ => true).ToListAsync();
+
+        public async Task<BookDTO?> GetAsync(string id) =>
+            await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(BookDTO newBook) =>
+            await _booksCollection.InsertOneAsync(newBook);
+
+        public async Task UpdateAsync(string id, BookDTO updatedBook) =>
+            await _booksCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+
+        public async Task RemoveAsync(string id) =>
+            await _booksCollection.DeleteOneAsync(x => x.Id == id);
+
+    }
+
+}
